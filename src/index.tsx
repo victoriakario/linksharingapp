b42e82d69b5da2902f3a469afa9cc24e2c292ab7
Victoria Kariolic <niaskywalk@gmail.com>
2023-07-12 01:01:47 -0400

    Add Normalize.css to app

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "normalize.css";
+
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Element with id \"root\" was not found");
