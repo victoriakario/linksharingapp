d5b7c04c59ece1f34fe0a9b898744bb5990b4260
Victoria Kariolic <niaskywalk@gmail.com>
2023-07-15 01:12:14 -0400

    Fix font file issue; Add typography and globalstyles

---
+++ src/globalStyles.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+import InstrumentSans from "assets/fonts/InstrumentSans-VariableFont_wdthwght.ttf";
+import InstrumentSansItalic from "assets/fonts/InstrumentSans-Italic-VariableFont_wdthwght.ttf";
+
+const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: "Instrument Sans";
+    src: url(${InstrumentSans}) format("truetype");
+  }
+
+  @font-face {
+    font-family: "Instrument Sans";
+    src: url(${InstrumentSansItalic}) format("truetype");
+    font-style: italic;
+  }
+
+  :root {
+    --background-color: #fafafa;
+    --body-text-color: #737373;
+    --heading-text-color: #333;
+    --accent-color: #633cff;
+    --accent-hover-color: #beadff;
+    --accent-light-color: #efebff; //secondary active button color, place holder color
+    --light-text-color: #ffffff;
+    --input-border-color: #d9d9d9;
+    --alert-color: #ff3939;
+
+    --box-shadow-primary: 0px 0px 32px 0px rgba(99 60 255 / 25%);
+
+  }
+
+  html {
+    height: 100vh;
+  }
+
+  body {
+    font-family: 'Instrument Sans', sans-serif;
+    font-size: 16px;
+  }
+
+`;
+
+export default GlobalStyle;
