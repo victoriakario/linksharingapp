ef58b73f5850a61e9c920985097a751a2a007417
Victoria Kariolic <victoria.kariolic@gmail.com>
2023-07-16 22:21:20 -0400

    Add secondary button variant option

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,21 @@
-import styled from "styled-components";
-import { headingSmall } from "./Typography";
+import styled, { css } from "styled-components";
+import { headingSmall } from "components/Typography";
 
-export const Button = styled.button`
+interface ButtonProps {
+  $variant?: "primary" | "secondary";
+}
+
+
+const secondaryStyles = css`
+  background-color: var(--light-text-color);
+  border: 1px solid var(--accent-color);
+  color: var(--accent-color);
+  &:active:not(:disabled) {
+    backgound-color: var(--accent-light-color);
+  }
+`;
+
+export const Button = styled.button<ButtonProps>`
   ${headingSmall};
   align-items: center;
   background-color: var(--accent-color);
@@ -12,18 +26,23 @@ export const Button = styled.button`
   display: flex;
   flex-direction: row;
   gap: .5rem;
-  height: 46px;
   justify-content: center;
   margin-bottom: 1.5rem;
+  outline: none;
+  padding: 11px 27px;
   width: 100%;
-  &:active:not(:disabled),
-  &:hover:not(:disabled) {
+
+  &:active:not(:disabled) {
     background-color: var(--accent-hover-color);
     box-shadow: var(--box-shadow-primary);
   }
-  &:disabled{
+
+  ${({$variant = "primary"}): typeof secondaryStyles | null => {
+    return $variant === "secondary" ? secondaryStyles : null;
+  }}
+
+  &:disabled {
     opacity: .25;
     cursor: not-allowed;
   }
 `;
-
